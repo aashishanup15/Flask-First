760721af7e0865071f7143b795a9f8aeea1711cb
ashishanup15 <aashishanupam3@gmail.com>
2024-08-02 11:40:10 +0530

    VoteEligibility

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('login.html')
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return 'Hello %s!' % name
 
+def login_success(name, x):
+    if x>=18: 
+        return 'You can Vote'
+    else:
+        return 'You Cannot Vote'
+    
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        age= int(request.form['age'])
+        return login_success(name=user, x=age) 
+        
+    # else:
+    #     user = request.args.get('username')
+    #     return redirect(url_for('login_success', name=user))
 if __name__ == '__main__':
     app.run()
